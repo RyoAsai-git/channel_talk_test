e33df45ddd134a8de26b306a6a2ea21be260d099
Ryo Asai <fu.orangina0516@iCloud.com>
2022-04-04 17:48:11 +0900

    feature:vueルートで画面作成

--- channel_talk_test/src/App.vue
+++ channel_talk_test/src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <img src="./assets/logo.png">
+    <router-view/> -->
+    <Header />
+    <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 export default {
-  name: 'App'
+  components: {
+    Header,
+    Footer
+  }
 }
 </script>
 
--- channel_talk_test/src/router/index.js
+++ channel_talk_test/src/router/index.js
@@ -1,15 +1,27 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Contact from "@/pages/Contact";
+import Profile from "@/pages/Profile";
+import Top from "@/pages/Top";
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "Top",
+      component: Top
+    },
+    {
+      path: "/profile",
+      name: "Profile",
+      component: Profile
+    },
+    {
+      path: "/contact",
+      name: "Contact",
+      component: Contact
     }
   ]
-})
+});
