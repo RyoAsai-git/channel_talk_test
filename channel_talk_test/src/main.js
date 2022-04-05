ebf9b0068f5592571135001eae5e917fccf2d794
Ryo Asai <fu.orangina0516@iCloud.com>
2022-04-05 10:49:37 +0900

    feature:チャネルトーク導入

--- channel_talk_test/src/main.js
+++ channel_talk_test/src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import bootChannelTalk from './channelTalk/channelTalk'
 
 Vue.config.productionTip = false
 
@@ -10,6 +11,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  bootChannelTalk,
   components: { App },
   template: '<App/>'
 })
